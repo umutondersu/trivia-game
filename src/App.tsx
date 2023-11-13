4917e76ac55a7ebd4d68844d58018faebbfdd152
Umut Önder <umutsonder@gmail.com>
2023-11-13 16:59:46 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+	return <h1 className="text-def">Test</h1>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				def: "var(--text)",
+				background: "var(--background)",
+				primary: "var(--primary)",
+				secondary: "var(--secondary)",
+				accent: "var(--accent)",
+			},
+
+			fontSize: {
+				sm: "0.750rem",
+				base: "1rem",
+				xl: "1.333rem",
+				"2xl": "1.777rem",
+				"3xl": "2.369rem",
+				"4xl": "3.158rem",
+				"5xl": "4.210rem",
+			},
+			fontFamily: {
+				heading: "Poppins",
+				body: "Poppins",
+			},
+			fontWeight: {
+				normal: "400",
+				bold: "700",
+			},
+		},
+	},
+	plugins: [],
+};
