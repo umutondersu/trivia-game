576b9dbd8a50334e301bf33c92c7a8b559815cca
Umut Önder <umutsonder@gmail.com>
2023-11-22 19:13:41 +0100

    Quizform Styling

--- src/components/ui/QuizForm.tsx
+++ src/components/ui/QuizForm.tsx
@@ -1,7 +1,6 @@
 import { Button } from "./button";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import * as z from "zod";
 import {
 	Form,
 	FormControl,
@@ -20,6 +19,7 @@ import {
 } from "./select";
 import { Input } from "./input";
 import { FormSchema, TFormValues } from "../../lib/types";
+import { cn } from "../../lib/utils";
 
 function QuizForm() {
 	const form = useForm<TFormValues>({
@@ -35,160 +35,165 @@ function QuizForm() {
 	}
 
 	return (
-		<Form {...form}>
-			<form
-				onSubmit={form.handleSubmit(onSubmit)}
-				className="flex flex-col">
-				<div className="flex flex-initial justify-evenly">
-					<FormField
-						control={form.control}
-						name="difficulty"
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Difficulty</FormLabel>
-								<Select
-									onValueChange={field.onChange}
-									defaultValue={field.value}>
+		<div>
+			<Form {...form}>
+				<form
+					onSubmit={form.handleSubmit(onSubmit)}
+					className="flex flex-col">
+					<div className="flex flex-initial justify-evenly mb-20 -mt-20 -mx-10">
+						<FormField
+							control={form.control}
+							name="difficulty"
+							render={({ field }) => (
+								<FormItem className="basis-2/12">
+									<FormLabel>Difficulty</FormLabel>
+									<Select
+										onValueChange={field.onChange}
+										defaultValue={field.value}>
+										<FormControl>
+											<SelectTrigger>
+												<SelectValue placeholder="Select a difficulty" />
+											</SelectTrigger>
+										</FormControl>
+										<SelectContent>
+											<SelectItem value="easy">
+												Easy
+											</SelectItem>
+											<SelectItem value="medium">
+												Medium
+											</SelectItem>
+											<SelectItem value="hard">
+												Hard
+											</SelectItem>
+										</SelectContent>
+									</Select>
+									<FormDescription>
+										Please choose a difficulty
+									</FormDescription>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="category"
+							render={({ field }) => (
+								<FormItem className="basis-2/12">
+									<FormLabel>Category</FormLabel>
+									<Select
+										onValueChange={field.onChange}
+										defaultValue={field.value}>
+										<FormControl>
+											<SelectTrigger>
+												<SelectValue placeholder="Select a category" />
+											</SelectTrigger>
+										</FormControl>
+										<SelectContent>
+											<SelectItem value="1">
+												Entertainment: Books
+											</SelectItem>
+											<SelectItem value="2">
+												Entertainment: Film
+											</SelectItem>
+											<SelectItem value="3">
+												Entertainment: Music
+											</SelectItem>
+											<SelectItem value="4">
+												Entertainment: Musicals &amp;
+												Theatres
+											</SelectItem>
+											<SelectItem value="5">
+												Entertainment: Television
+											</SelectItem>
+											<SelectItem value="6">
+												Entertainment: Video Games
+											</SelectItem>
+											<SelectItem value="7">
+												Entertainment: Board Games
+											</SelectItem>
+											<SelectItem value="8">
+												Science & Nature
+											</SelectItem>
+											<SelectItem value="9">
+												Science: Computers
+											</SelectItem>
+											<SelectItem value="10">
+												Science: Mathematics
+											</SelectItem>
+											<SelectItem value="11">
+												Mythology
+											</SelectItem>
+											<SelectItem value="12">
+												Sports
+											</SelectItem>
+											<SelectItem value="13">
+												Geography
+											</SelectItem>
+											<SelectItem value="14">
+												History
+											</SelectItem>
+											<SelectItem value="15">
+												Politics
+											</SelectItem>
+											<SelectItem value="16">
+												Art
+											</SelectItem>
+											<SelectItem value="17">
+												Celebrities
+											</SelectItem>
+											<SelectItem value="18">
+												Animals
+											</SelectItem>
+											<SelectItem value="19">
+												Vehicles
+											</SelectItem>
+											<SelectItem value="20">
+												Entertainment: Comics
+											</SelectItem>
+											<SelectItem value="21">
+												Science: Gadgets
+											</SelectItem>
+											<SelectItem value="22">
+												Entertainment: Japanese Anime &
+												Manga
+											</SelectItem>
+											<SelectItem value="23">
+												Entertainment: Cartoon &
+												Animations
+											</SelectItem>
+										</SelectContent>
+									</Select>
+									<FormDescription>
+										Please choose a category
+									</FormDescription>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="numberOfQuestions"
+							render={({ field }) => (
+								<FormItem className="basis-2/12">
+									<FormLabel>Number of Questions</FormLabel>
 									<FormControl>
-										<SelectTrigger>
-											<SelectValue placeholder="Select a difficulty" />
-										</SelectTrigger>
+										<Input
+											placeholder="e.g 10"
+											type="number"
+											{...field}
+										/>
 									</FormControl>
-									<SelectContent>
-										<SelectItem value="easy">
-											Easy
-										</SelectItem>
-										<SelectItem value="medium">
-											Medium
-										</SelectItem>
-										<SelectItem value="hard">
-											Hard
-										</SelectItem>
-									</SelectContent>
-								</Select>
-								<FormDescription>
-									Please choose a difficulty
-								</FormDescription>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					<FormField
-						control={form.control}
-						name="category"
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Category</FormLabel>
-								<Select
-									onValueChange={field.onChange}
-									defaultValue={field.value}>
-									<FormControl>
-										<SelectTrigger>
-											<SelectValue placeholder="Select a category" />
-										</SelectTrigger>
-									</FormControl>
-									<SelectContent>
-										<SelectItem value="1">
-											Entertainment: Books
-										</SelectItem>
-										<SelectItem value="2">
-											Entertainment: Film
-										</SelectItem>
-										<SelectItem value="3">
-											Entertainment: Music
-										</SelectItem>
-										<SelectItem value="4">
-											Entertainment: Musicals &amp;
-											Theatres
-										</SelectItem>
-										<SelectItem value="5">
-											Entertainment: Television
-										</SelectItem>
-										<SelectItem value="6">
-											Entertainment: Video Games
-										</SelectItem>
-										<SelectItem value="7">
-											Entertainment: Board Games
-										</SelectItem>
-										<SelectItem value="8">
-											Science & Nature
-										</SelectItem>
-										<SelectItem value="9">
-											Science: Computers
-										</SelectItem>
-										<SelectItem value="10">
-											Science: Mathematics
-										</SelectItem>
-										<SelectItem value="11">
-											Mythology
-										</SelectItem>
-										<SelectItem value="12">
-											Sports
-										</SelectItem>
-										<SelectItem value="13">
-											Geography
-										</SelectItem>
-										<SelectItem value="14">
-											History
-										</SelectItem>
-										<SelectItem value="15">
-											Politics
-										</SelectItem>
-										<SelectItem value="16">Art</SelectItem>
-										<SelectItem value="17">
-											Celebrities
-										</SelectItem>
-										<SelectItem value="18">
-											Animals
-										</SelectItem>
-										<SelectItem value="19">
-											Vehicles
-										</SelectItem>
-										<SelectItem value="20">
-											Entertainment: Comics
-										</SelectItem>
-										<SelectItem value="21">
-											Science: Gadgets
-										</SelectItem>
-										<SelectItem value="22">
-											Entertainment: Japanese Anime &
-											Manga
-										</SelectItem>
-										<SelectItem value="23">
-											Entertainment: Cartoon & Animations
-										</SelectItem>
-									</SelectContent>
-								</Select>
-								<FormDescription>
-									Please choose a category
-								</FormDescription>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					<FormField
-						control={form.control}
-						name="numberOfQuestions"
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Number of Questions</FormLabel>
-								<FormControl>
-									<Input
-										placeholder="e.g 10"
-										type="number"
-										{...field}
-									/>
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-				</div>
-				<Button className="self-center" type="submit">
-					Start the Game
-				</Button>
-			</form>
-		</Form>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+					</div>
+					<Button className="self-center p-8 text-lg" type="submit">
+						Start the Game
+					</Button>
+				</form>
+			</Form>
+		</div>
 	);
 }
 
