7dcb9ff13be9f7e983a84d18a0257886a3ce7ff2
Umut Önder <umutsonder@gmail.com>
2023-11-28 13:37:54 +0100

    changed icons with lucide

--- src/components/Questions/AnswerStatusBar.tsx
+++ src/components/Questions/AnswerStatusBar.tsx
@@ -2,20 +2,32 @@ import { cn } from "../../lib/utils";
 import Check from "../../assets/circle-check-regular.svg";
 import { useAtomValue } from "jotai";
 import { AnswerStatusAtom } from "../../lib/atoms/Questions";
+import { CheckCircle2, XCircle } from "lucide-react";
 
 function AnswerStatusBar() {
 	const answered = useAtomValue(AnswerStatusAtom);
+	const ClassName = "relative inline top-[1px] text-foreground";
 	//TODO: Change Icons with Lucide React Icons
 	return (
 		<div
 			className={cn(
-				"relative bottom-3 mx-auto flex items-center justify-center gap-x-1 rounded-sm bg-green-600 p-2 opacity-0 md:bottom-6",
+				"relative bottom-3 mx-auto flex w-60 items-center justify-center gap-x-1 rounded-sm bg-red-600 p-2 opacity-0 md:bottom-6 md:w-80",
 				{
-					"opacity-100": answered,
+					"opacity-100": answered.answered,
+					"bg-green-600": answered.correct === true,
 				},
 			)}>
-			<img className="inline h-5 w-5" src={Check} alt="Check Icon" />
-			Correct or Incorrect Answer
+			{answered.correct ? (
+				<>
+					<CheckCircle2 className={ClassName} size={25} />
+					Correct Answer!
+				</>
+			) : (
+				<>
+					<XCircle className={ClassName} size={25} />
+					Incorrect Answer!
+				</>
+			)}
 		</div>
 	);
 }
--- src/components/ui/togglebutton.tsx
+++ src/components/ui/togglebutton.tsx
@@ -1,7 +1,6 @@
 import { Button } from "./button";
-import MoonIconSrc from "../../assets/moon.svg";
-import SunIconSrc from "../../assets/sun.svg";
 import { theme } from "../../lib/definitions";
+import { Sun, Moon } from "lucide-react";
 
 function ToggleButton({
 	theme,
@@ -10,15 +9,22 @@ function ToggleButton({
 	theme: theme;
 	toggleTheme: () => void;
 }) {
+	const ClassName =
+		"transition-all ease-linear duration-150 active:scale-50 active:translate-y-1 text-[hsl(222.2,84%,4.9%)]";
 	return (
 		<Button
 			onClick={toggleTheme}
-			className="w-10 h-10 rounded-full p-0 m-3 absolute top-0 right-0">
-			<img
+			className="absolute right-0 top-0 m-3 h-10 w-10 rounded-full p-0">
+			{theme === "dark" ? (
+				<Sun className={ClassName} />
+			) : (
+				<Moon className={ClassName} />
+			)}
+			{/* <img
 				className="transition-all ease-linear duration-150 active:scale-50 active:translate-y-1"
 				src={theme === "dark" ? SunIconSrc : MoonIconSrc}
 				alt="toggle theme Icon"
-			/>
+			/> */}
 		</Button>
 	);
 }
