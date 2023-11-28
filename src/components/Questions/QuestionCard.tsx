752157dc33e887f78b9450bde9d32fd8ffdfbfc2
Umut Önder <umutsonder@gmail.com>
2023-11-28 13:56:43 +0100

    Rearrange classNames in QuestionCard component

--- src/components/Questions/QuestionCard.tsx
+++ src/components/Questions/QuestionCard.tsx
@@ -6,9 +6,9 @@ import { Skeleton } from "../ui/skeleton";
 export default function QuestionCard() {
 	const [Question] = useAtom(loadableQuestionAtom);
 	return (
-		<Card className="grid place-items-center relative md:mx-40 mx-9 md:h-40 h-52 text-center">
+		<Card className="relative mx-9 grid h-52 place-items-center text-center md:mx-40 md:h-40">
 			{Question.state === "loading" ? (
-				<Skeleton className="w-11/12 h-1/5" />
+				<Skeleton className="h-1/5 w-11/12" />
 			) : (
 				<CardHeader>
 					<CardTitle>
