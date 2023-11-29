b2b7ffb5e21c3e8128adab53afb7f52a3f0f361a
Umut Önder <umutsonder@gmail.com>
2023-11-29 22:11:17 +0100

    better variable names for ProgressBar

--- src/components/Questions/ProgressBar.tsx
+++ src/components/Questions/ProgressBar.tsx
@@ -3,12 +3,12 @@ import { Progress } from "../ui/progress";
 import { useAtomValue } from "jotai";
 
 export default function ProgressBar() {
-	const Bar = useAtomValue(ProgressBarAtom);
+	const CurrentProgress = useAtomValue(ProgressBarAtom);
 	const questionCount = useAtomValue(QuestionCountAtom);
-	const CurrentProgress = (Bar / questionCount) * 100;
+	const Bar = (CurrentProgress / questionCount) * 100;
 	return (
 		<div className="absolute left-2 top-5 h-2.5 w-3/4 md:w-1/3">
-			<Progress value={CurrentProgress} />
+			<Progress value={Bar} />
 		</div>
 	);
 }
