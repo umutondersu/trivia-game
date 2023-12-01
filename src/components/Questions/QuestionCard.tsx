333d9175c743ba359831eab7448b797e3c105ed3
Umut Önder <umutsonder@gmail.com>
2023-12-01 15:36:49 +0100

    responsive design improvement

--- src/components/Questions/QuestionCard.tsx
+++ src/components/Questions/QuestionCard.tsx
@@ -6,7 +6,7 @@ import { Skeleton } from "../ui/skeleton";
 export default function QuestionCard() {
 	const [Question] = useAtom(loadableQuestionAtom);
 	return (
-		<Card className="relative mx-9 grid h-52 place-items-center text-center md:mx-40 md:h-40">
+		<Card className="relative mx-9 grid h-52 place-items-center text-center sm:top-3 sm:h-44 md:mx-40 md:h-40">
 			{Question.state === "loading" ? (
 				<Skeleton className="h-1/5 w-11/12" />
 			) : (
